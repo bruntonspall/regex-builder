f826fcc9fe53b2ebd878bab09308ded0335e1d84
Michael Brunton-Spall <michael@brunton-spall.co.uk>
2009-12-18 12:28:49 +0000

    Rewrote builder to use parse tree
    Now on each successive call, it adds a node to the tree.
    This will hopefully make the stringify much easier

--- builder.py
+++ builder.py
@@ -1,89 +1,170 @@
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+    def __str__(self):
+        if self.next:
+            return str(self.next)
+        return ""
+    def __len__(self):
+        if self.next:
+            return len(self.next)
+        return 0
+
+class LiteralNode(Node):
+    def __str__(self):
+        return Node.__str__(self)+self.data
+    def __len__(self):
+        return Node.__len__(self)+len(self.data.replace('\\',''))
+        
+class RepeatsMinMaxNode(Node):
+    def __str__(self):
+        regex = self.data['regex']
+        if len(regex) > 1:
+            self.data['regex'] = group(regex, non_capture=True)
+        return Node.__str__(self)+"%(regex)s{%(min)d,%(max)d}" % self.data
+    def __len__(self):
+        return len(self.data['regex'])
+
+class RepeatsNumNode(Node):
+    def __str__(self):
+        regex = self.data['regex']
+        if len(regex) > 1:
+            self.data['regex'] = group(regex, non_capture=True)
+        return Node.__str__(self)+"%(regex)s{%(num)d}" % self.data
+    def __len__(self):
+        return len(self.data['regex'])
+
+class OneOrMoreNode(Node):
+    def __str__(self):
+        if len(self.data) > 1:
+            self.data = group(self.data, non_capture=True)
+        return Node.__str__(self)+"%s+" % str(self.data)
+    def __len__(self):
+        return len(self.data)
+
+class ZeroOrMoreNode(Node):
+    def __str__(self):
+        if len(self.data) > 1:
+            self.data = group(self.data, non_capture=True)
+        return Node.__str__(self)+"%s*" % str(self.data)
+    def __len__(self):
+        return len(self.data)
+
+class GroupNode(Node):
+    def __str__(self):
+        if self.data['lazy']:
+            return Node.__str__(self)+"(%(regex)s?)" % self.data
+        return Node.__str__(self)+"(%(regex)s)" % self.data
+    def __len__(self):
+        return len(self.data['regex'])
+
+class NonCaptureGroupNode(Node):
+    def __str__(self):
+        if self.data['lazy']:
+            return Node.__str__(self)+"(?:%(regex)s?)" % self.data
+        return Node.__str__(self)+"(?:%(regex)s)" % self.data
+    def __len__(self):
+        return len(self.data['regex'])
+
+class RangeNode(Node):
+    def __str__(self):
+        return Node.__str__(self)+"[%s]" % str(self.data)
+    def __len__(self):
+        return 1
+
+class InvertedRangeNode(Node):
+    def __str__(self):
+        return Node.__str__(self)+"[^%s]" % str(self.data)
+    def __len__(self):
+        return 1
+
 class RegexBuilder:
     def __init__(self):
         self.text = ""
         self.len = 0
+        self.root = Node()
+        self.tail = self.root
+
+    def __insert_node(self, node):
+        node.next = self.root
+        self.root = node
 
     def literal(self, lit):
         """ Adds a text literal to the regular expression """
-        self.text += lit
-        self.len += len(lit.replace('\\', ''))
+        self.__insert_node(LiteralNode(lit))
         return self
 
     def repeats(self, regex, min, max=None):
         """ Repeats the passed expression n times """
         if max:
-            self.text += self.__group_and_append(regex, '{%d,%d}' % (min, max))
+            self.__insert_node(RepeatsMinMaxNode({'min':min, 'max':max, 'regex': regex}))
         else:
-            self.text += self.__group_and_append(regex, '{%d}' % (min))
-        self.len += len(regex)
+            self.__insert_node(RepeatsNumNode({'num':min, 'regex': regex}))
         return self
     
     def group(self, regex, non_capture=False, lazy=False):
         """ Groups the passed regex with a backlink """
-        pre = ''
-        post = ''
         if non_capture:
-            pre = '?:'
-        if lazy:
-            post = '?'
-        self.text += "(%s%s%s)" % (pre, regex.to_string(), post)
-        self.len += len(regex)
+            self.__insert_node(NonCaptureGroupNode({'regex': regex, 'lazy': lazy}))
+        else:
+            self.__insert_node(GroupNode({'regex': regex, 'lazy': lazy}))
         return self
 
     def one_or_more(self, regex):
         """ Repeats the passed expression one or more times """
-        self.text += self.__group_and_append(regex, '+')
-        self.len += len(regex)
+        self.__insert_node(OneOrMoreNode(regex))
         return self
 
     def zero_or_more(self, regex):
         """ Repeats the passed expression zero or more times """
-        self.text += self.__group_and_append(regex, '*')
-        self.len += len(regex)
+        self.__insert_node(ZeroOrMoreNode(regex))
         return self
     
     def range(self, range):
         """ Matches any characters in the range """
-        self.text += "[%s]" % (range)
-        self.len += 1
+        self.__insert_node(RangeNode(range))
         return self
 
     def inverted_range(self, range):
         """ Matches any characters in the range """
-        self.text += "[^%s]" % (range)
-        self.len += 1
+        self.__insert_node(InvertedRangeNode(range))
         return self
 
-    def __group_and_append(self, regex, lit):
-        r = regex.to_string()
-        if len(regex) > 1:
-            r = "(?:%s)" % (r)
-        return "%s%s" % (r,lit)
-        
-
     def to_string(self):
-        return self.text
+        """ DEPRECATED: use str(regex) instead """
+        return str(self)
+
+    def __str__(self):
+        return str(self.root)
 
     def __len__(self):
-        return self.len
+        return len(self.root)
 
 def literal(lit):
+    """ Adds a text literal to the regular expression """
     return RegexBuilder().literal(lit)
 
 def repeats(regex, min=None, max=None):
+    """ Repeats the passed expression n times """
     return RegexBuilder().repeats(regex, min, max)
 
-def group(regex, non_capture=False):
-    return RegexBuilder().group(regex, non_capture)
+def group(regex, non_capture=False, lazy=False):
+    """ Groups the passed regex with a backlink """
+    return RegexBuilder().group(regex, non_capture, lazy)
 
 def one_or_more(regex):
+    """ Repeats the passed expression one or more times """
     return RegexBuilder().one_or_more(regex)
 
 def zero_or_more(regex):
+    """ Repeats the passed expression zero or more times """
     return RegexBuilder().zero_or_more(regex)
 
 def range(lit):
+    """ Matches any characters in the range """
     return RegexBuilder().range(lit)
 
 def inverted_range(lit):
+    """ Matches any characters NOT in the range """
     return RegexBuilder().inverted_range(lit)
--- regex_test.py
+++ regex_test.py
@@ -36,28 +36,85 @@ class BuilderTests(unittest.TestCase):
     def test_module_functions(self):
         self.assertEquals('ab(c{3,7})d', literal('ab').group(repeats(literal('c'), 3, 7)).literal('d').to_string())
 
+class StringAndLenTests(unittest.TestCase):
+    def test_literal(self):
+        regex = literal('ab')
+        self.assertEquals(2, len(regex))
+        self.assertEquals('ab', str(regex))
+    
+    def test_literal(self):
+        regex = literal('ab').literal('c')
+        self.assertEquals(3, len(regex))
+        self.assertEquals('abc', str(regex))
+    
+    def test_repeats(self):
+        regex = repeats(literal('a'),1)
+        self.assertEquals(1, len(regex))
+        self.assertEquals('a{1}', str(regex))
+        regex = repeats(literal('a'),1,3)
+        self.assertEquals(1, len(regex))
+        self.assertEquals('a{1,3}', str(regex))
+
+    def test_one_or_more(self):
+        regex = one_or_more(literal('a'))
+        self.assertEquals(1, len(regex))
+        self.assertEquals('a+', str(regex))
+
+    def test_zero_or_more(self):
+        regex = zero_or_more(literal('a'))
+        self.assertEquals(1, len(regex))
+        self.assertEquals('a*', str(regex))
+
+    def test_group(self):
+        regex = group(literal('a'))
+        self.assertEquals('(a)', str(regex))
+        self.assertEquals(1, len(regex))
+
+    def test_non_capture_group(self):
+        regex = group(literal('a'), non_capture=True)
+        self.assertEquals('(?:a)', str(regex))
+        self.assertEquals(1, len(regex))
+
+    def test_lazy_group(self):
+        regex = group(literal('a'), lazy=True)
+        self.assertEquals('(a?)', str(regex))
+        self.assertEquals(1, len(regex))
+
+    def test_lazy_non_capture_group(self):
+        regex = group(literal('a'), non_capture=True, lazy=True)
+        self.assertEquals('(?:a?)', str(regex))
+        self.assertEquals(1, len(regex))
+
+    def test_range(self):
+        regex = range('abc')
+        self.assertEquals('[abc]', str(regex))
+        self.assertEquals(1, len(regex))
+
+    def test_inverted_range(self):
+        regex = inverted_range('abc')
+        self.assertEquals('[^abc]', str(regex))
+        self.assertEquals(1, len(regex))
+
+
 class ComplexTests(unittest.TestCase):
     def test_one_or_more_multiword_literals(self):
-        self.assertEquals('a(?:bc)+d', literal('a').one_or_more(literal('bc')).literal('d').to_string())
+        self.assertEquals('(?:bc)+', one_or_more(literal('bc')).to_string())
     def test_zero_or_more_multiword_literals(self):
-        self.assertEquals('a(?:bc)*d', literal('a').zero_or_more(literal('bc')).literal('d').to_string())
+        self.assertEquals('(?:bc)*', zero_or_more(literal('bc')).to_string())
     def test_repetition_of_multiword_literals(self):
-        self.assertEquals('a(?:bc){2,3}d', literal('a').repeats(literal('bc'), 2, 3).literal('d').to_string())
+        self.assertEquals('(?:bc){2,3}', repeats(literal('bc'), 2, 3).to_string())
     def test_one_or_more_single_character_literals(self):
         self.assertEquals(r'a\d+d', literal('a').one_or_more(literal(r'\d')).literal('d').to_string())
     def test_html_tag_parser(self):
         """ www.regular-expressions.info/examples.html - says this <TAG\b[^>]*>(.*?)</TAG> will match html tag TAG """
+        regex = literal(r'<TAG\b').zero_or_more(
+                        inverted_range('>')
+                    ).literal('>').group(
+                        zero_or_more(
+                            literal('.')), lazy=True
+                    ).literal('</TAG>')
         self.assertEquals(
-            r'<TAG\b[^>]*>(.*?)</TAG>', 
-            literal(r'<TAG\b')
-            .zero_or_more(
-                inverted_range('>'))
-            .literal('>')
-            .group(
-                zero_or_more(
-                    literal('.')), lazy=True)
-            .literal('</TAG>')
-            .to_string())
+            r'<TAG\b[^>]*>(.*?)</TAG>', regex.to_string())
     def test_simple_ip_address(self):
         """ www.regular-expressions.info/examples.html - says this \d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} will match tags """
         self.assertEquals(
